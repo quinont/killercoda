32e9fb76512a0d84208a5c7a8ccce617d45d84b6
Juan Esteban Colcombet <quinont@gmail.com>
2022-07-22 19:32:23 -0400

    Actualizando el check

--- desafio04/verify.sh
+++ desafio04/verify.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 for i in {1..5}; do
-    curl host01:30000/mypath
+    [[ "$(curl host01:30000/mypath -s)" == "app1" ]] || exit 2
     sleep 0.3
 done
 
 for i in {1..5}; do
-    curl host01:31000/mypath
+    [[ "$(curl host01:31000/mypath -s)" == "app2" ]] || exit 2
     sleep 0.3
 done
 
