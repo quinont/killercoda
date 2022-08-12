f8b2d92a21fb791d23a4537f8830e47ffdfdff25
Juan Esteban Colcombet <quinont@gmail.com>
2022-08-12 19:17:33 -0400

    Fix menores del lugar de los archivos y una espera del deploy del metrics server

--- resources101/init/foreground.sh
+++ resources101/init/foreground.sh
@@ -1,3 +1,6 @@
 FILE=/ks/wait-background.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
 
+echo "esperando por el deploy del metric server..."
+kubectl rollout status deploy/metrics-server -n kube-system
+
 echo "Todo listo"
